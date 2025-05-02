ea605197b4fbcb04864131188ec503512b11b896
haeuniiii <whgkdms020506@gmail.com>
2025-05-03 01:14:01 +0900

    수정사항 추가해주세요

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/Hello.java
@@ -0,0 +1,7 @@
+package me.shinsunyoung.springbootdeveloper;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, jaehyuk and sungyeon!");
+    }
+}
