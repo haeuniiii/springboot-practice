519d267c59459527d51817fb6499ac3d3a005e0d
haeuniiii <whgkdms020506@gmail.com>
2025-05-17 23:04:32 +0900

    스프링부트서버 실행해보려구
    뭐가 문제일까

--- src/main/java/me/shinsunyoung/springbootdeveloper/SpringbootDeveloperApplication.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/SpringbootDeveloperApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDeveloperApplication {
-    public static void main(String[] args){
-        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+public class SpringbootDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDeveloperApplication.class, args);
     }
 }
